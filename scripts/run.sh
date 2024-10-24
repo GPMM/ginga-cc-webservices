7c010191dca0611d09ec8e003d77ce0891af2343
Joel dos Santos <jsantos@eic.cefet-rj.br>
2024-10-24 00:09:58 -0300

    organizing scripts and samples

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,17 @@
 export NODE_ENV=development
 export GINGACCWSPORT=44642
 
-export SERVER_URL='192.168.68.103'
+export SERVER_URL='192.168.0.xx'
 # export GINGA_SOCKET_PORT=
 
 # export APP_ID='100'
-# export APP_BASE_URL='../appSET'
+# export APP_BASE_URL='../appNCL'
 # export DEV_DEFAULT_UUID=123456
 
 # export USER_FILE_PATH='../userAPI'
 # export CURRENT_USER='f626b4f2-d989-4fbf-9645-f830fd12b348'
 # export CURRENT_SERVICE='midiacom'
 
-export REMOTE_DEV_FILE_PATH='/gingaFiles/remote-devices/remoteDevices.json'
+export REMOTE_DEV_FILE_PATH='../remoteDevices.json'
 
 npm start
